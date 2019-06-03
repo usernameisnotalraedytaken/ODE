e13c09fc77217cc2695c703472d29c59884a3598
usernameisnotalraedytaken <42826126+usernameisnotalraedytaken@users.noreply.github.com>
2019-06-03 10:36:58 +0800

    Update ODE.cpp

--- ODE.cpp
+++ ODE.cpp
@@ -23,7 +23,7 @@ double RK4(double x, double y, double h)
 	double K1 = f(x, y);
 	double K2 = f(x + h / 2.0, y + h / 2.0 * K1);
 	double K3 = f(x + h / 2.0, y + h / 2.0 * K2);
-	double K4 = f(x + h, y + h * K1);
+	double K4 = f(x + h, y + h * K3);
 	double Yn = y + h / 6.0 * (K1 + 2.0 * K2 + 2.0 * K3 + K4);
 	return Yn;
 }
